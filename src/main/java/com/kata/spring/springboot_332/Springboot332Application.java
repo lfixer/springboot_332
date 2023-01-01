77e53b726830237d6d04e28d390f4990efee51c8
lfixer <strmgddn@gmail.com>
2023-01-02 00:52:29 +0300

    3.3.2

---
+++ src/main/java/com/kata/spring/springboot_332/Springboot332Application.java
@@ -0,0 +1,13 @@
+package com.kata.spring.springboot_332;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot332Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot332Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kata/spring/springboot_332/Springboot332ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.spring.springboot_332;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot332ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
